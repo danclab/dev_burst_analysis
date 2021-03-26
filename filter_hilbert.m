88dd97f79fb41499e0b388968d1ea2c2caae8b89
James Bonaiuto <jbonaiuto@gmail.com>
2021-03-26 12:52:56 +0100

    added padding

--- filter_hilbert.m
+++ filter_hilbert.m
@@ -17,11 +17,21 @@ function [filtered_data, amp]=filter_hilbert(data, srate, foi)
 % Example: 
 %   [filtered_data, amp]= filter_hilbert(exp_data, 250, [13 30])
 
+% Pad data to avoid edge artifacts
+% Pad with dc offset
+dc=mean(data);
+% Pad with 1s on either side
+padded_data=[repmat(dc, srate, 1); data; repmat(dc, srate, 1)];
+
 % Bandpass filter in frequency range
 % 6th order two-pass Butterworth filter
 % reduce order in case of instability
-filtered_data = ft_preproc_bandpassfilter(data', srate, foi, 6, 'but',...
-    'twopass', 'reduce')';
+filtered_data = ft_preproc_bandpassfilter(padded_data', srate, foi, 6,...
+    'but', 'twopass', 'reduce')';
 
 % Get amplitude from Hilbert transform
 amp=abs(hilbert(filtered_data));
+
+% Get rid of padding
+filtered_data=filtered_data(srate+1:srate+size(data,1),:);
+amp=amp(srate+1:srate+size(data,1),:);
