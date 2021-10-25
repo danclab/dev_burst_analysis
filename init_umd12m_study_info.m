f25b75cfc67a410a85a75f6214bedeb5c068c3f8
James Bonaiuto <jbonaiuto@gmail.com>
2021-10-25 15:59:28 +0200

    updated band info

--- init_umd12m_study_info.m
+++ init_umd12m_study_info.m
@@ -2,10 +2,11 @@
 function study_info=init_umd12m_study_info()
 
 study_info=[];
+study_info.age='12m';
 
 %% Specify Paths
 % Locate data on hard drive
-study_info.data_dir = '../../data/UMD/12m/bids';
+study_info.data_dir = '../../data/12m/';
 study_info.deriv_dir = '../../data/UMD/12m/deriv';
 
 %% Subject list
@@ -14,7 +15,8 @@ study_info.participant_info=readtable([fullfile(study_info.data_dir, 'participan
 
 % Electrode clusters to look in
 study_info.clusters={'C3','C4'};
-study_info.cluster_channels={{'E16', 'E20', 'E21', 'E22'},...
+study_info.cluster_channels={
+    {'E16', 'E20', 'E21', 'E22'},...
     {'E41', 'E49', 'E50', 'E51'}};
 
 % Conditions and events
@@ -22,12 +24,6 @@ study_info.conditions={'observe','execute'};
 study_info.baseline_evts={'OBBASE_P','EXBASE_P'};
 study_info.exp_evts={'FTGO_P','FTGE_P'};
 
-study_info.beta_band=[12 18];
-study_info.beta_thresh_sd=1.25;
-study_info.mu_band=[6 10];
-
-% Open EEGlab
-[ALLEEG, EEG, CURRENTSET] = eeglab;
-
-% Init fieldtrip
-ft_defaults;
+study_info.beta_band=[13.5 17.5; 14.00 18.00];
+study_info.beta_thresh_sd=[1.5 1.2];
+study_info.alpha_band=[6.5 8.5; 6.0 9.00];
--- init_umdadult_study_info.m
+++ init_umdadult_study_info.m
@@ -2,10 +2,11 @@
 function study_info=init_umdadult_study_info()
 
 study_info=[];
+study_info.age='adult';
 
 %% Specify Paths
 % Locate data on hard drive
-study_info.data_dir = '../../data/UMD/adult/bids';
+study_info.data_dir = '../../data/adult';
 study_info.deriv_dir = '../../data/UMD/adult/deriv';
 
 %% Subject list
@@ -14,7 +15,8 @@ study_info.participant_info=readtable([fullfile(study_info.data_dir, 'participan
 
 % Electrode clusters to look in
 study_info.clusters={'C3','C4'};
-study_info.cluster_channels={{'E16', 'E20', 'E21', 'E22'},...
+study_info.cluster_channels={
+    {'E16', 'E20', 'E21', 'E22'},...
     {'E41', 'E49', 'E50', 'E51'}};
 
 % Conditions and events
@@ -22,12 +24,6 @@ study_info.conditions={'observe','execute'};
 study_info.baseline_evts={'OBBASE_P','EXBASE_P'};
 study_info.exp_evts={'FTGO_P','FTGE_P'};
 
-study_info.beta_band=[18 26];
-study_info.beta_thresh_sd=1.15;
-study_info.mu_band=[9 13];
-
-% Open EEGlab
-[ALLEEG, EEG, CURRENTSET] = eeglab;
-
-% Init fieldtrip
-ft_defaults;
+study_info.beta_band=[19.5 25.5; 18.75 24.25];
+study_info.beta_thresh_sd=[1.1 1.2];
+study_info.alpha_band=[9.5 12.5; 9.5 12.5];
